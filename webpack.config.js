feed84aacfef0a12b3cd28cfe7be0969c8200426
Alex <raven-xl@mail.ru>
2020-11-15 19:04:36 +0300

    some changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path')
 
 
 module.exports = {
-    context: path.resolve(__dirname, 'source'),
+    context: path.resolve(__dirname, 'source'),  //смотрит за исходниками в папке source
     mode: 'development',
     entry: './index.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: 'bundle.js',  //место всех будующих скриптов
+        path: path.resolve(__dirname, 'dist')  //прописывает путь где это находится
     }
 }
