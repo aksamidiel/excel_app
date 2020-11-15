42feb9937f332682b6edc221bee937a43a0fc6b0
Alex <raven-xl@mail.ru>
2020-11-15 19:32:51 +0300

    config plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
@@ -6,7 +8,14 @@ module.exports = {
     mode: 'development',
     entry: './index.js',
     output: {
-        filename: 'bundle.js',  //место всех будующих скриптов
+        filename: 'bundle.[hash].js',  //место всех будующих скриптов
         path: path.resolve(__dirname, 'dist')  //прописывает путь где это находится
-    }
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+
+    ]
 }
